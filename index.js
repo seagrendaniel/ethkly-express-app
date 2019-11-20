b95c9801ccbc2ea4233fe788907ea203a404a5f4
blackdanamite <seagrendaniel562@gmail.com>
2019-11-20 13:46:30 -0800

    product json displays

--- index.js
+++ index.js
@@ -6,107 +6,115 @@ const cookie = require('cookie');
 const nonce = require('nonce')();
 const querystring = require('querystring');
 const request = require('request-promise');
-const Shopify = require('shopify-api-node');
+const ShopifyAPI = require('shopify-api-node');
 const ShopifyToken = require('shopify-token');
+
 
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
-const scopes = 'read_products, read_collection_listings';
+const scopes = 'write_products, read_products, read_collection_listings';
 const forwardingAddress = "https://1e9c341e.ngrok.io"; // Replace this with your HTTPS Forwarding address
+// const nonceState = nonce();
 
-const shopify = new Shopify({
-    shopName: 'ethkly-green.myshopify.com',
-    apiKey: apiKey,
-    
-})
+
+// const shopify = new ShopifyAPI({
+//     shop: 'ethkly-green.myshopify.com',
+//     shopify_api_key: apiKey, // Your API key
+//     shopify_shared_secret: apiSecret, // Your Shared Secret
+//     shopify_scope: 'write_products, read_products, read_collection_listings',
+//     redirect_uri: forwardingAddress + 'ethkly-green.myshopify.com',
+//     nonce: nonceState
+// })
 
 app.get('/shopify', (req, res) => {
     const shop = req.query.shop;
     if (shop) {
-      const state = nonce();
-      const redirectUri = forwardingAddress + '/shopify/callback';
-      const installUrl = 'https://' + shop +
-        '/admin/oauth/authorize?client_id=' + apiKey +
-        '&scope=' + scopes +
-        '&state=' + state +
-        '&redirect_uri=' + redirectUri;
-  
-      res.cookie('state', state);
-      res.redirect(installUrl);
-    } else {
-      return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
-    }
-  });
+        const state = nonce();
+        const redirectUri = forwardingAddress + '/shopify/callback';
+        //   const redirectUri = forwardingAddress + '/views/home';
+        const installUrl = 'https://' + shop +
+            '/admin/oauth/authorize?client_id=' + apiKey +
+            '&scope=' + scopes +
+            '&state=' + state +
+            '&redirect_uri=' + redirectUri;
 
-  app.get('/shopify/callback', (req, res) => {
+        res.cookie('state', state);
+        res.redirect(installUrl);
+    } else {
+        return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+    }
+});
+
+app.get('/shopify/callback', (req, res) => {
     const { shop, hmac, code, state } = req.query;
     const stateCookie = cookie.parse(req.headers.cookie).state;
-  
+
     if (state !== stateCookie) {
-      return res.status(403).send('Request origin cannot be verified');
+        return res.status(403).send('Request origin cannot be verified');
     }
-  
+
     if (shop && hmac && code) {
-      // DONE: Validate request is from Shopify
-      const map = Object.assign({}, req.query);
-      delete map['signature'];
-      delete map['hmac'];
-      const message = querystring.stringify(map);
-      const providedHmac = Buffer.from(hmac, 'utf-8');
-      const generatedHash = Buffer.from(
-        crypto
-          .createHmac('sha256', apiSecret)
-          .update(message)
-          .digest('hex'),
-          'utf-8'
+        // DONE: Validate request is from Shopify
+        const map = Object.assign({}, req.query);
+        delete map['signature'];
+        delete map['hmac'];
+        const message = querystring.stringify(map);
+        const providedHmac = Buffer.from(hmac, 'utf-8');
+        const generatedHash = Buffer.from(
+            crypto
+                .createHmac('sha256', apiSecret)
+                .update(message)
+                .digest('hex'),
+            'utf-8'
         );
-      let hashEquals = false;
-  
-      try {
-        hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
-      } catch (e) {
-        hashEquals = false;
-      };
-  
-      if (!hashEquals) {
-        return res.status(400).send('HMAC validation failed');
-      }
-  
-      // DONE: Exchange temporary code for a permanent access token
-      const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-      const accessTokenPayload = {
-        client_id: apiKey,
-        client_secret: apiSecret,
-        code,
-      };
-  
-      request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-      .then((accessTokenResponse) => {
-        const accessToken = accessTokenResponse.access_token;
-        // DONE: Use access token to make API call to 'shop' endpoint
-        const shopRequestUrl = 'https://' + shop + '/admin/api/2019-10/shop.json';
-        const shopRequestHeaders = {
-          'X-Shopify-Access-Token': accessToken,
+        let hashEquals = false;
+
+        try {
+            hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+        } catch (e) {
+            hashEquals = false;
         };
-  
-        request.get(shopRequestUrl, { headers: shopRequestHeaders })
-        .then((shopResponse) => {
-          res.status(200).end(shopResponse);
-        })
-        .catch((error) => {
-          res.status(error.statusCode).send(error.error.error_description);
-        });
-      })
-      .catch((error) => {
-        res.status(error.statusCode).send(error.error.error_description);
-      });
-  
+
+        if (!hashEquals) {
+            return res.status(400).send('HMAC validation failed');
+        }
+
+        // DONE: Exchange temporary code for a permanent access token
+        const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+        const accessTokenPayload = {
+            client_id: apiKey,
+            client_secret: apiSecret,
+            code,
+        };
+
+        request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+            .then((accessTokenResponse) => {
+                const accessToken = accessTokenResponse.access_token;
+                // DONE: Use access token to make API call to 'shop' endpoint
+                const shopRequestUrl = 'https://' + shop + '/admin/api/2019-10/products.json?since_id=632910392';
+                // const shopRequestUrl = 'https://' + shop + '/views/home.ejs';
+                const shopRequestHeaders = {
+                    'X-Shopify-Access-Token': accessToken,
+                };
+
+                request.get(shopRequestUrl, { headers: shopRequestHeaders })
+                    .then((shopResponse) => {
+                        res.status(200).end(shopResponse);
+                    })
+                    .catch((error) => {
+                        res.status(error.statusCode).send(error.error.error_description);
+                    });
+            })
+            .catch((error) => {
+                res.status(error.statusCode).send(error.error.error_description);
+            });
+
     } else {
-      res.status(400).send('Required parameters missing');
+        res.status(400).send('Required parameters missing');
     }
-  });
+});
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
 
